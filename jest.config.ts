f5e2bed60a9e9650ecf0fca842ad98ed4fafd73e
Bennett Smrdel <bennett@midwestdiesel.com>
2024-06-20 09:57:44 -0500

    test: Add jest tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  transform: {
+    "^.+\\.tsx?$": "babel-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testEnvironment: "node",
+  transformIgnorePatterns: ["/node_modules/"],
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1"
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
 };
