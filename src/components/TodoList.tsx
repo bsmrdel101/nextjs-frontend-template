5cbf2c4c8a7f8b6d8601129bd6f633cef040c582
Bennett Smrdel <bennett@midwestdiesel.com>
2024-06-20 09:47:44 -0500

    feat: Create todo list

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,40 @@
+export const getCompletedTasksCount = (tasks: Task[]) => {
+  return tasks.reduce((acc, cur) => acc + (cur.completed ? 1 : 0), 0);
+};
+
+
 export default function TodoList() {
+  const tasks: Task[] = [
+    {
+      id: 1,
+      name: 'Break the dishes',
+      completed: true,
+    },
+    {
+      id: 2,
+      name: 'Burn the lawn',
+      completed: false,
+    },
+    {
+      id: 3,
+      name: 'Scream into the sky',
+      completed: false,
+    },
+  ];
+
+
   return (
-    <div className="todo-list">
+    <div className="todo-list" style={{ margin: 'auto', textAlign: 'center' }}>
       <h1>Todo List</h1>
+      <div className="todo-list__tasks" style={{ display: 'flex', flexDirection: 'column', gap: '0.8rem' }}>
+        {tasks.map((task: Task) => {
+          return (
+            <div key={task.id} className="todo-list__task-item" style={{ backgroundColor: 'var(--grey-dark-3)', padding: '1rem', borderRadius: '0.5rem' }}>
+              <h3>{ task.name }</h3>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- tests/todo.test.ts
+++ tests/todo.test.ts
@@ -1,1 +1,28 @@
+import { expect } from '@jest/globals';
+import { getCompletedTasksCount } from "@/components/TodoList";
 
+const tasks: Task[] = [
+  {
+    id: 1,
+    name: 'Break the dishes',
+    completed: true,
+  },
+  {
+    id: 2,
+    name: 'Burn the lawn',
+    completed: false,
+  },
+  {
+    id: 3,
+    name: 'Scream into the sky',
+    completed: false,
+  },
+];
+
+
+describe('Todo List', () => {
+  it('Gets a list of completed tasks', () => {
+    const count = getCompletedTasksCount(tasks);
+    expect(count).toEqual(1);
+  });
+});
